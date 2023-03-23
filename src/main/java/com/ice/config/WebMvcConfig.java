6e9873a7b3743a2867b54e300a8c8da25cc0cc31
Ice <wangice@petalmail.com>
2023-03-23 22:40:30 +0800

    项目配置完毕

---
+++ src/main/java/com/ice/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.ice.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @Title: ReggieApplication
+ * @Auth: Ice
+ * @Date: 2023/3/23 22:14
+ * @Version: 1.0
+ * @Desc: Set up static resource mappings
+ */
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("addResourceHandlers");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
