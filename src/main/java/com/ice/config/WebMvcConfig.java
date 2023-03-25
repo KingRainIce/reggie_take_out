4313c7d558a2e787ad46ad9a9e8165593dfd5609
Ice <wangice@petalmail.com>
2023-03-25 21:46:55 +0800

    修改员工状态

--- src/main/java/com/ice/config/WebMvcConfig.java
+++ src/main/java/com/ice/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.ice.config;
 
+import com.ice.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * @Title: ReggieApplication
@@ -22,5 +27,16 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("addResourceHandlers");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // Create a message converter
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        // Set up the object converter, and the underlying layer uses Jackson to convert Java to json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        // Append the above message converter object to the transformer collection of the MVC framework
+        converters.add(0,messageConverter);
+        super.extendMessageConverters(converters);
     }
 }
